f3ad688fb7afe8d80e6f85b177c93514041c013a
imldy <im.ldy@outlook.com>
2025-04-14 21:04:37 +0800

    feat: 问题选项中增加时间属性，用于后续可能的选项排序

--- backend/src/main/java/lsgwr/exam/entity/QuestionOption.java
+++ backend/src/main/java/lsgwr/exam/entity/QuestionOption.java
@@ -4,10 +4,12 @@
 package lsgwr.exam.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Data
 @Entity
@@ -16,4 +18,16 @@ public class QuestionOption {
     private String questionOptionId;
     private String questionOptionContent;
     private String questionOptionDescription;
+    /**
+     * 创建时间, 设计表时设置了自动插入当前时间，无需在Java代码中设置了
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 更新时间，设计表时设置了自动插入当前时间，无需在Java代码中设置了。
+     * 同时@DynamicUpdate注解可以时间当数据库数据变化时自动更新，无需人工维护
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
 }
