ebd1de01beb32880669001caa3f7d9bc7eecdac2
imldy <im.ldy@outlook.com>
2025-04-14 22:25:20 +0800

    feat: 增加试卷的其他几部分枚举对象

--- backend/src/main/java/lsgwr/exam/enums/QuestionEnum.java
+++ backend/src/main/java/lsgwr/exam/enums/QuestionEnum.java
@@ -13,7 +13,15 @@ public enum QuestionEnum {
      */
     RADIO(1, "单选题"),
     CHECK(2, "多选题"),
-    JUDGE(3, "判断题");
+    JUDGE(3, "判断题"),
+    PART_I(411, "写作题"),
+    PART_II_A(511, "听力A部分"),
+    PART_II_B(521, "听力B部分"), 
+    PART_II_C(531, "听力C部分"),
+    PART_III_A(611, "阅读A部分"),
+    PART_III_B(621, "阅读B部分"),
+    PART_III_C(631, "阅读C部分"),
+    PART_IV(711, "翻译题");
 
 
     QuestionEnum(Integer id, String role) {
